41fffb37035fd9421b5a084f9b8218a76ec7e9bb
epacik <epaciatko@gmail.com>
2021-02-24 04:03:51 +0100

    Working InputDialog with arbitrary number of inputs
    Usage:
    var output = await Dialogs.InputDialog.Show(
    				"Window Title",
    				"Dialog header",
    				new Dialogs.InputDialogEntry[] {
    					new Dialogs.InputDialogEntry("Input 1", false),
    					new Dialogs.InputDialogEntry("Input 2", true),
    			});

---
+++ Yaggi.Desktop/Dialogs/InputDialog.axaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using System.Threading.Tasks;
+using Yaggi.Desktop.Extensions;
+using Avalonia.Interactivity;
+using System.Linq;
+using Avalonia.Layout;
+
+namespace Yaggi.Desktop.Dialogs
+{
+	public class InputDialog : Window
+	{
+		/// <summary>
+		/// Pls don't use this
+		/// </summary>
+		public InputDialog()
+		{
+			InitializeComponent();
+#if DEBUG
+			this.AttachDevTools();
+#endif
+		}
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		private void CancelButton_Click(object sender, RoutedEventArgs args)
+		{
+			Close(null);
+		}
+
+		private void ConfirmButton_Click(object sender, RoutedEventArgs args)
+		{
+			
+			var dc = (ViewModels.InputDialogViewModel)DataContext;
+			//get input as strings from TextBoxes, if textbox text was null, replace it with empty string
+			Close(dc.Items.Where(x => x.GetType() == typeof(TextBox)).Select(x => ((TextBox)x).Text ?? "").ToArray());
+		}
+
+
+		/// <summary>
+		/// Shows dialog and returns user input
+		/// </summary>
+		/// <code>
+		/// var output = await Dialogs.InputDialog.Show(
+		///		"Window Title",
+		///		"Dialog header",
+		///		new Dialogs.InputDialogEntry[] {
+		///			new Dialogs.InputDialogEntry("Input 1", false),
+		///			new Dialogs.InputDialogEntry("Input 2", true),
+		///	});
+		/// </code>
+		///
+		/// <param name="title">Title of the dialog</param>
+		/// <param name="header">Header of the dialog</param>
+		/// <param name="inputEntries">Array of Input entries</param>
+		/// <returns>
+		/// Array of user inputs (length of that array is equal to <paramref name="inputEntries"/>)<br/>
+		/// null if user clicked cancel or closed the window
+		/// </returns>
+		public static async Task<string[]> Show(string title, string header, InputDialogEntry[] inputEntries)
+		{
+			
+			var dlg = new InputDialog();
+			if (inputEntries is null || inputEntries.Length == 0)
+			{
+				throw new ArgumentException($"Parameter \"{nameof(inputEntries)}\" of type {typeof(InputDialogEntry[])} was null or empty");
+			}
+			//fill the dialog
+			var dc = new ViewModels.InputDialogViewModel();
+			dlg.DataContext = dc;
+			dc.Title = title;
+			dc.Header = header;
+			dc.InputEntries = inputEntries;
+
+			return await dlg.ShowDialog<string[]>();
+		}
+	}
+
+	/// <summary>
+	/// Represents a single entry in <see cref="InputDialog"/> window
+	/// </summary>
+	public struct InputDialogEntry
+	{
+		/// <summary>
+		/// Represents a single entry in <see cref="InputDialog"/> window
+		/// </summary>
+		/// <param name="label">Description of the input</param>
+		/// <param name="maskInput">Should be set to true if input contain a sensitive data such as password or API key</param>
+		public InputDialogEntry(string label, bool maskInput)
+		{
+			Label = label;
+			MaskInput = maskInput;
+		}
+		public readonly string Label;
+		public readonly bool MaskInput;
+	}
+}
---
+++ Yaggi.Desktop/ViewModels/InputDialogViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Avalonia.Collections;
+using Avalonia.Controls;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Yaggi.Desktop.Dialogs;
+
+namespace Yaggi.Desktop.ViewModels
+{
+	public class InputDialogViewModel : ViewModelBase
+	{
+
+		/// <summary>
+		/// Title of the window
+		/// </summary>
+		public string Title
+		{
+			get => title;
+			set => this.RaiseAndSetIfChanged(ref title, value);
+		}
+		private string title;
+
+		/// <summary>
+		/// Whether header should be visible or not
+		/// </summary>
+		public bool HeaderVisible => !string.IsNullOrWhiteSpace(Header);
+
+		/// <summary>
+		/// Header text
+		/// </summary>
+		public string Header
+		{
+			get => header;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref header, value);
+				this.RaisePropertyChanged(nameof(HeaderVisible));
+			}
+		}
+		private string header;
+
+		/// <summary>
+		/// Set inputs
+		/// </summary>
+		public IEnumerable<InputDialogEntry> InputEntries
+		{
+			set
+			{
+				//create an enumerable of alternating textboxes and textblocks
+				//(for each item in InputEntries create one TextBlock for an description and a TextBox for the user input)
+				var items = value
+					.Select(ie => new TextBlock { Text = ie.Label })
+					.Zip(
+						value.Select(ie => new TextBox {
+							// show '*' instead of letters if that input should have masked input
+							PasswordChar = ie.MaskInput ? '*' : '\0',
+							RevealPassword = ie.MaskInput
+						}),
+						(l, m) => new Control[] { l, m })
+					.SelectMany(x => x);
+				//remove any existing items and populate Items
+				Items.Clear();
+				Items.AddRange(items);
+			}
+		}
+
+		/// <summary>
+		/// List of alternating textboxes and textblocks <br/>
+		/// More info: <see cref="InputEntries"/>
+		/// </summary>
+		public AvaloniaList<Control> Items
+		{
+			get => items;
+			set => this.RaiseAndSetIfChanged(ref items, value);
+		}
+		private AvaloniaList<Control> items = new AvaloniaList<Control>();
+
+		public string Input { get; set; }
+	}
+}
