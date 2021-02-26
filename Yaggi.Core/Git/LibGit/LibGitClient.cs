3924eecc21b6485502401512c8cb47569c0a438a
petris <paprikapolishgamer@gmail.com>
2021-02-26 15:59:13 +0100

    Fixes

--- Yaggi.Core/Git/LibGit/LibGitClient.cs
+++ Yaggi.Core/Git/LibGit/LibGitClient.cs
@@ -59,82 +59,99 @@ namespace Yaggi.Core.Git.LibGit
 						if (!string.IsNullOrEmpty(remoteUrl))
 							prompt += $" for \"{remoteUrl}\"";
 						string usernameFromUrl = Marshal.PtrToStringUTF8(fromUrl);
-						switch (types)
+
+						// ReSharper disable HeapView.BoxingAllocation
+						if (types.HasFlag(GitCredentialType.UserpassPlaintext))
 						{
-							case GitCredentialType.UserpassPlaintext:
-								(bool, string[]) input = authenticationProvider(prompt, new[]
-								{
-									("Username", usernameFromUrl, false),
-									("Password", (string)null, true)
-								});
-								if (!input.Item1)
-									return GitErrorCode.User;
-								NativeString userName = new(input.Item2[0], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => userName.Dispose());
-								NativeString password = new(input.Item2[1], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => password.Dispose());
-								return GitNative.CredentialUserpassPlaintextNew(data, userName.Data, password.Data);
-							case GitCredentialType.SshKey:
-								string pubPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh", "id_rsa.pub");
-								string privPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh", "id_rsa");
-								(bool, string[]) input2 = authenticationProvider(prompt, new[]
-								{
-									("Username", usernameFromUrl, false),
-									("Public key path", File.Exists(pubPath) ? pubPath : null, false),
-									("Private key path", File.Exists(privPath) ? privPath : null, false),
-									("Passphrase", (string)null, true)
-								});
-								if (!input2.Item1)
-									return GitErrorCode.User;
-								NativeString userName2 = new(input2.Item2[0], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => userName2.Dispose());
-								NativeString publickey = new(input2.Item2[1], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => publickey.Dispose());
-								NativeString privatekey = new(input2.Item2[2], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => privatekey.Dispose());
-								NativeString passphrase = new(input2.Item2[3], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => passphrase.Dispose());
-								return GitNative.CredentialSshKeyNew(data, userName2.Data, publickey.Data, privatekey.Data, passphrase.Data);
-							case GitCredentialType.Default:
-								return GitNative.CredentialDefaultNew(data);
-							case GitCredentialType.Username:
-								(bool, string[]) input3 = authenticationProvider(prompt, new[] { ("Username", usernameFromUrl, false) });
-								if (!input3.Item1)
-									return GitErrorCode.User;
-								NativeString userName3 = new(input3.Item2[0], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => userName3.Dispose());
-								return GitNative.CredentialUsernameNew(data, userName3.Data);
-							case GitCredentialType.SshMemory:
-								string pubPath2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh", "id_rsa.pub");
-								string privPath2 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh", "id_rsa");
-								(bool, string[]) input4 = authenticationProvider(prompt, new[]
-								{
-									("Username", usernameFromUrl, false),
-									("Public key path", File.Exists(pubPath2) ? File.ReadAllText(pubPath2) : null, false),
-									("Private key path", File.Exists(privPath2) ? File.ReadAllText(privPath2) : null, false),
-									("Passphrase", (string)null, true)
-								});
-								if (!input4.Item1)
-									return GitErrorCode.User;
-								NativeString userName4 = new(input4.Item2[0], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => userName4.Dispose());
-								NativeString publickey2 = new(input4.Item2[1], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => publickey2.Dispose());
-								NativeString privatekey2 = new(input4.Item2[2], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => privatekey2.Dispose());
-								NativeString passphrase2 = new(input4.Item2[3], StringEncoding.UTF8, true);
-								freeCallbacks.Add(() => passphrase2.Dispose());
-								return GitNative.CredentialSshKeyMemoryNew(data, userName4.Data, publickey2.Data, privatekey2.Data, passphrase2.Data);
-							// ReSharper disable RedundantCaseLabel
-							// TODO: implement
-							case GitCredentialType.SshCustom:
-							//return GitNative.CredentialSshCustomNew(data);
-							case GitCredentialType.SshInteractive:
-							//return GitNative.CredentialSshInteractiveNew(data);
-							// ReSharper restore RedundantCaseLabel
-							default:
+							(bool, string[]) input = authenticationProvider(prompt, new[]
+							{
+								("Username", usernameFromUrl, false),
+								("Password", (string)null, true)
+							});
+							if (!input.Item1)
 								return GitErrorCode.User;
+							NativeString userName = new(input.Item2[0], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => userName.Dispose());
+							NativeString password = new(input.Item2[1], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => password.Dispose());
+							return GitNative.CredentialUserpassPlaintextNew(data, userName.Data, password.Data);
 						}
+
+						if (types.HasFlag(GitCredentialType.Default))
+							return GitNative.CredentialDefaultNew(data);
+
+						if (types.HasFlag(GitCredentialType.Username))
+						{
+							(bool, string[]) input3 =
+								authenticationProvider(prompt, new[] { ("Username", usernameFromUrl, false) });
+							if (!input3.Item1)
+								return GitErrorCode.User;
+							NativeString userName3 = new(input3.Item2[0], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => userName3.Dispose());
+							return GitNative.CredentialUsernameNew(data, userName3.Data);
+						}
+
+						if (types.HasFlag(GitCredentialType.SshKey))
+						{
+							string pubPath =
+								Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh",
+									"id_rsa.pub");
+							string privPath =
+								Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh",
+									"id_rsa");
+							(bool, string[]) input2 = authenticationProvider(prompt, new[]
+							{
+								("Username", usernameFromUrl, false),
+								("Public key path", File.Exists(pubPath) ? pubPath : null, false),
+								("Private key path", File.Exists(privPath) ? privPath : null, false),
+								("Passphrase", (string)null, true)
+							});
+							if (!input2.Item1)
+								return GitErrorCode.User;
+							NativeString userName2 = new(input2.Item2[0], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => userName2.Dispose());
+							NativeString publickey = new(input2.Item2[1], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => publickey.Dispose());
+							NativeString privatekey = new(input2.Item2[2], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => privatekey.Dispose());
+							NativeString passphrase = new(input2.Item2[3], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => passphrase.Dispose());
+							return GitNative.CredentialSshKeyNew(data, userName2.Data, publickey.Data, privatekey.Data,
+								passphrase.Data);
+						}
+
+						if (types.HasFlag(GitCredentialType.SshMemory))
+						{
+							string pubPath2 =
+								Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh",
+									"id_rsa.pub");
+							string privPath2 =
+								Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ssh",
+									"id_rsa");
+							(bool, string[]) input4 = authenticationProvider(prompt, new[]
+							{
+								("Username", usernameFromUrl, false),
+								("Public key path", File.Exists(pubPath2) ? File.ReadAllText(pubPath2) : null, false),
+								("Private key path", File.Exists(privPath2) ? File.ReadAllText(privPath2) : null,
+									false),
+								("Passphrase", (string)null, true)
+							});
+							if (!input4.Item1)
+								return GitErrorCode.User;
+							NativeString userName4 = new(input4.Item2[0], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => userName4.Dispose());
+							NativeString publickey2 = new(input4.Item2[1], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => publickey2.Dispose());
+							NativeString privatekey2 = new(input4.Item2[2], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => privatekey2.Dispose());
+							NativeString passphrase2 = new(input4.Item2[3], StringEncoding.UTF8, true);
+							freeCallbacks.Add(() => passphrase2.Dispose());
+							return GitNative.CredentialSshKeyMemoryNew(data, userName4.Data, publickey2.Data,
+								privatekey2.Data, passphrase2.Data);
+						}
+						// ReSharper restore HeapView.BoxingAllocation
+
+						return GitErrorCode.User;
 					};
 					GCHandle handle1 = GCHandle.Alloc(acquireCredentials, GCHandleType.Normal);
 					freeCallbacks.Add(() => handle1.Free());
