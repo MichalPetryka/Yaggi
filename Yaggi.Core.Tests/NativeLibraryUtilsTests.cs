a9fb40f4830f70f369e9165583e79137e64ff3db
petris <paprikapolishgamer@gmail.com>
2021-03-03 17:13:18 +0100

    Simplify test

--- Yaggi.Core.Tests/NativeLibraryUtilsTests.cs
+++ Yaggi.Core.Tests/NativeLibraryUtilsTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Xunit;
 using Yaggi.Core.Marshaling;
@@ -8,16 +7,10 @@ namespace Yaggi.Core.Tests
 {
 	public class NativeLibraryUtilsTests
 	{
-		public static IEnumerable<object[]> ValidCases =>
-			new List<object[]>
-			{
-				new object[] { null, null, "Kernel32.dll", "libc.so", "libc.dylib", "libc" },
-				new object[] { "Kernel32.dll", "libc.so", "libc.dylib", "libc", null, null },
-				new object[] { "Kernel32.dll", "ntdll.dll", "libc.so", "libdl.so", "libc.dylib", "libdl.dylib", "libc", "libdl" },
-			};
-
 		[Theory]
-		[MemberData(nameof(ValidCases))]
+		[InlineData(null, null, "Kernel32.dll", "libc.so", "libc.dylib", "libc")]
+		[InlineData("Kernel32.dll", "libc.so", "libc.dylib", "libc", null, null)]
+		[InlineData("Kernel32.dll", "ntdll.dll", "libc.so", "libdl.so", "libc.dylib", "libdl.dylib", "libc", "libdl")]
 		public void LoadTest(params string[] names)
 		{
 			IntPtr module = NativeLibraryUtils.LoadAny(names);
